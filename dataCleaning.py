1cd501e854a21efaa484f4a7b86e9a336155fd31
MichaelC609 <mjcastillo@cpp.edu>
2024-04-25 20:35:32 -0700

    created scatterplots comparing G1, G2 and G3

--- dataCleaning.py
+++ dataCleaning.py
@@ -1,8 +1,24 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #load the dataset
-df = pd.read_csv('student-por.csv')
-print(df.isnull())	#dataset contains no missing values
-
-df2 = pd.read_csv('student-mat.csv')
+df = pd.read_csv('student-mat(in).csv')
+print(df)
 print(df.isnull())	#check for missing values on 2nd dataset
+
+#scatter plots comparing G1, G2 and G3
+G1G2Plot = sns.scatterplot(data=df, x='G1', y='G2')
+G1G2Plot.set_xlabel('G1')
+G1G2Plot.set_ylabel('G2')
+plt.show()
+
+G1G3Plot = sns.scatterplot(data=df, x='G1', y='G3')
+G1G2Plot.set_xlabel('G1')
+G1G2Plot.set_ylabel('G3')
+plt.show()
+
+G2G3Plot = sns.scatterplot(data=df, x='G2', y='G3')
+G1G2Plot.set_xlabel('G2')
+G1G2Plot.set_ylabel('G3')
+plt.show()
